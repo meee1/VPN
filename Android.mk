d3d2802a273678845d7ada4b193da0187309ec8c
Michael Oborne <mich146@hotmail.com>
2024-02-01 16:15:32 +1100

    add Android.mk

---
+++ Android.mk
@@ -0,0 +1,39 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := src/client.c src/vpn_config.c src/vpn_registry.c lib/crypto.c
+
+LOCAL_MODULE := vpnclient
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SHARED_LIBRARIES := libc libm libdl libssl libcrypto
+
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/includes
+
+LOCAL_CFLAGS := \
+    -std=gnu11 -g -Wall -Wextra -O2 \
+
+LOCAL_LDFLAGS := -Wl,-export-dynamic -Wl,--no-gc-sections
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := src/server.c src/vpn_config.c src/vpn_registry.c lib/crypto.c
+
+LOCAL_MODULE := vpnserver
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SHARED_LIBRARIES := libc libm libdl libssl libcrypto
+
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/includes
+
+LOCAL_CFLAGS := \
+    -std=gnu11 -g -Wall -Wextra -O2 \
+
+LOCAL_LDFLAGS := -Wl,-export-dynamic -Wl,--no-gc-sections
+
+include $(BUILD_EXECUTABLE)
